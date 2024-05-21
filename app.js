d8aa53d11a7b3418ccd67ce5d0c136f3a4b3fddc
Rudraksh Singh <you@example.com>
2024-05-21 14:15:31 +0530

    Topi-01

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1"),
+    React.createElement("h2", {}, "i am h2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "i am h1"),
+    React.createElement("h2", {}, "i am h2"),
+  ]),
+]);
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
